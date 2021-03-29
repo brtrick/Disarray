8da51aab5a6f42fe6ce4b1ba9339303454bcdffa
Marco Torre <marco.a.torre@gmail.com>
2021-03-29 15:18:07 -0400

    Add login, signup, nav components

---
+++ frontend/src/components/nav/navbar.js
@@ -0,0 +1,44 @@
+import React from 'react';
+import { Link } from 'react-router-dom'
+
+class NavBar extends React.Component {
+    constructor(props) {
+        super(props);
+        this.logoutUser = this.logoutUser.bind(this);
+        this.getLinks = this.getLinks.bind(this);
+    }
+
+    logoutUser(e) {
+        e.preventDefault();
+        this.props.logout();
+    }
+
+    // Selectively render links dependent on whether the user is logged in
+    getLinks() {
+        if (this.props.loggedIn) {
+            return (
+                <div>
+                    <Link to={'/'}>Logged In NavBar</Link>
+                    <button onClick={this.logoutUser}>Logout</button>
+                </div>
+            );
+        } else {
+            return (
+                <div>
+                    <Link to={'/'}>Logged Out NavBar</Link>
+                </div>
+            );
+        }
+    }
+
+    render() {
+        return (
+            <div>
+                <h1>Disarray Logo</h1>
+                { this.getLinks()}
+            </div>
+        );
+    }
+}
+
+export default NavBar;
