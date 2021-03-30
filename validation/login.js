36e327aa0d3998c9e0026794114cdd234dbfb2ec
Marco Torre <marco.a.torre@gmail.com>
2021-03-29 22:05:29 -0400

    Resolve username error validation

--- validation/login.js
+++ validation/login.js
@@ -11,7 +11,7 @@ module.exports = function validateLoginInput(data){
     }
     
     if (Validator.isEmpty(data.password)) {
-        errors.username = "Password is required";
+        errors.password = "Password is required";
     }
 
     return {
