a31d874a7a8104e7cdfe88892f021d44dd9e365a
Brad Trick <btrick@gmail.com>
2021-03-29 22:29:20 -0400

    Convert methods to Wordlist class

---
+++ gameLogic/wordlist.js
@@ -0,0 +1,57 @@
+import lineByLine from "n-readlines";
+// const lineByLine = require("n-readlines");
+
+export default class Wordlist {
+// class Wordlist {
+
+    constructor (filename) {
+        this.wordlist = {};
+        this.prefixlist = {};
+        this.initializeLists(filename);
+    }
+
+    initializeLists (filename) {
+        const fileLine = new lineByLine(filename);
+        
+        let word;
+
+        while (word = fileLine.next()) {
+            const score = this.calculateWordValue (word.length);
+            this.wordlist[word.toString('ascii')] = score;
+            this.prefixlist[word.slice(0,3).toString('ascii')] = true;
+        }
+    }
+
+    //checkAnswers modifies foundWords to indicate which words are valid
+    //Returns the total score
+    checkAnswers (foundWords) {
+        const words = Object.keys(foundWords);
+        let score = 0;
+
+        words.forEach((word) => {
+            if (this.wordlist[word]) {
+                foundWords[word] = true;
+                score += this.wordlist[word];
+            }
+            // If foundWords not initialized to false, need to set it here
+            // else foundWords[word] = false;
+        });
+        return score;
+    }
+
+    calculateWordValue (wordLength) {
+        switch (wordLength) {
+            case 3:
+            case 4:
+                return 1;
+            case 5:
+                return 2;
+            case 6:
+                return 3;
+            case 7:
+                return 5;
+            default:
+                return 11;
+        }
+    }
+}
