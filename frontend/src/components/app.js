96f7aec10a04f186b77c5c29a9d95f5099bc8c0a
Marco Torre <marco.a.torre@gmail.com>
2021-04-02 11:26:02 -0400

    Resolve routing issues

--- frontend/src/components/app.js
+++ frontend/src/components/app.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { AuthRoute } from '../util/route_util';
+import { AuthRoute, ProtectedRoute } from '../util/route_util';
 import { Switch, Route } from 'react-router-dom';
 import NavBarContainer from './nav/navbar_container';
 
@@ -15,11 +15,11 @@ const App = () => (
         <Modal/>
         <NewGameModal />
         <NavBarContainer />
+        <Route exact path="/" component={MainPage} />
+        <Route path="/board" component={BoardContainer}/>
         <Switch>
-            <AuthRoute exact path="/" component={MainPage} />
             <AuthRoute exact path="/login" component={LoginFormContainer} />
             <AuthRoute exact path="/signup" component={SignupFormContainer} />
-            <Route path="/board" component={BoardContainer}/>
         </Switch>
     </div>
 );
