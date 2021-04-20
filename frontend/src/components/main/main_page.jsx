eccbb6b29c3e334a5b8a01d021e48c891530b84c
alejandro <alejandroweil13@gmail.com>
2021-04-20 12:24:47 -0400

    Finish testing modal

--- frontend/src/components/main/main_page.jsx
+++ frontend/src/components/main/main_page.jsx
@@ -3,7 +3,6 @@ import { NavLink } from 'react-router-dom';
 import '../../stylesheets/splash.css';
 import '../../stylesheets/main.css';
 import '../../stylesheets/reset.css';
-import StartRound from '../newRoundModal/startRoundContainer';
 
 
 class MainPage extends React.Component {
@@ -12,7 +11,6 @@ class MainPage extends React.Component {
         return (
             <div className='splash-page'>
                 <NavLink to='/board' className='start'>Press Start</NavLink>
-                <StartRound/>
             </div>
         );
     }
