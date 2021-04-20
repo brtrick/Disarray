0e840da84aacea535e8b5b36e7c67d8825e7aacc
alejandro <alejandroweil13@gmail.com>
2021-04-20 12:12:39 -0400

    Make modal for between rounds close on timer

---
+++ frontend/src/components/newRoundModal/modalTimer.jsx
@@ -0,0 +1,58 @@
+import React from 'react';
+
+
+class ModalTimer extends React.Component {
+    constructor(props){
+        super(props);
+        this.state = {
+            seconds: 10
+        }
+
+        this.target = new Date().getTime() + this.state.seconds*1000;
+        this.updateTime = this.updateTime.bind(this);
+    }
+
+    updateTime = () => {
+        const timeLeft = this.target - (new Date());
+
+        if (timeLeft <= 0) {
+            this.setState({
+                seconds: 0
+            });
+            this.props.closeModal();
+        } else {
+            this.setState({
+                seconds: Math.floor((timeLeft/1000) % 60)
+            });
+            this.timeout = setTimeout(this.updateTime, 950);
+        }
+    }
+
+    componentDidMount() {
+        this.timeout = setTimeout(this.updateTime, 950);
+    }
+
+    componentWillUnmount() {
+        clearTimeout(this.timeout);
+    }
+
+    stringify = num => {
+        if (num < 10){
+            return "0" + num.toString();
+        } else {
+            return num;
+        }
+    }
+
+    render() {
+        return (
+            <div className='timer-container'>
+                <div className='timer-header'>New Round Starts In:</div>
+                <span>{this.stringify(this.state.seconds)}</span>
+            </div>
+        )
+    }
+
+}
+
+export default ModalTimer;
