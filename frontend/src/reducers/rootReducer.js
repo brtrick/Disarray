c3ef2d00087b234ae255015f08cc86e6f792cceb
Brad Trick <btrick@appacademy.io>
2022-03-16 00:18:13 -0400

    Add socket, board, and timer reducers

--- frontend/src/reducers/rootReducer.js
+++ frontend/src/reducers/rootReducer.js
@@ -3,12 +3,18 @@ import session from './session_reducer';
 import errors from './errors_reducer';
 import leaderboard from './leaderboard_reducer'
 import ui from "./ui_reducer";
+import socket from './socketReducer';
+import board from './boardReducer';
+import timer from './timerReducer';
 
 const RootReducer = combineReducers({
     session,
     ui,
     errors,
-    leaderboard
+    leaderboard,
+    socket,
+    board, 
+    timer
 });
 
 export default RootReducer;
