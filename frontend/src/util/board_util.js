1da4c11e87bc1477bb9464c6037fdf7fc7bd6804
Brad Trick <76491823+brtrick@users.noreply.github.com>
2024-06-19 18:06:47 -0400

    Prevent invalid moves using clicks

--- frontend/src/util/board_util.js
+++ frontend/src/util/board_util.js
@@ -1,10 +1,35 @@
 const validMove = ((lastMove, currentMove) => {
     const boardLength = 4;
+    const boardSize = boardLength * boardLength;
+    const onTop = lastMove < boardLength;
+    const onBottom = lastMove >= boardSize - boardLength;
+    const onLeft = lastMove % boardLength === 0;
+    const onRight = lastMove % boardLength === boardLength - 1;
 
-    const validMoves = [lastMove + 1, lastMove - 1,
-                        lastMove - boardLength, lastMove - boardLength + 1, lastMove - boardLength -1,
-                        lastMove + boardLength, lastMove + boardLength + 1, lastMove + boardLength -1                        
-                       ];
+    const validMoves = [];
+    
+    // Add moves if last move not on top row
+    if (!onTop) {
+      validMoves.push(lastMove - boardLength);
+      if (!onLeft)
+        validMoves.push(lastMove - boardLength - 1);
+      if (!onRight)
+        validMoves.push(lastMove - boardLength + 1);
+    }
+    // Add moves if last move not on bottom row
+    if (!onBottom) {
+      validMoves.push(lastMove + boardLength);
+      if (!onLeft)
+        validMoves.push(lastMove + boardLength - 1);
+      if (!onRight)
+        validMoves.push(lastMove + boardLength + 1);
+    }
+    // Add moves if not on sides
+    if (!onLeft)
+      validMoves.push(lastMove - 1);
+    if (!onRight)
+      validMoves.push(lastMove + 1);
+
     return validMoves.includes(currentMove);
 });
 
